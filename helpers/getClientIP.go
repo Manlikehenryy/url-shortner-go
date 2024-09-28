47a86d3dae2bf914b9fee140717f1d33d7d66f59
manlikehenryy <mbamaluhenry8@gmail.com>
2024-09-28 21:05:43 +0100

    log ip

--- helpers/getClientIP.go
+++ helpers/getClientIP.go
@@ -1,9 +1,17 @@
 package helpers
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetClientIP(c *gin.Context) string {
 	// Check for proxy headers (commonly used in production)
+	log.Println("X-Real-IP: "+c.Request.Header.Get("X-Real-IP"))
+	log.Println("X-Forwarded-For: "+c.Request.Header.Get("X-Forwarded-For"))
+	log.Println("ClientIP: "+c.ClientIP())
+	
 	ip := c.Request.Header.Get("X-Real-IP")
 	if ip == "" {
 		ip = c.Request.Header.Get("X-Forwarded-For")
